e15b69f889a92c8b7a8a4c6109a05386606d0c8b
Ryuujin <ryuujin@asora17.10k>
2018-04-08 14:51:17 -0400

    added beans & partial changes to curriculum

--- hydra-curriculum-service/src/main/java/com/revature/hydra/curriculum/beans/ScheduledDate.java
+++ hydra-curriculum-service/src/main/java/com/revature/hydra/curriculum/beans/ScheduledDate.java
@@ -2,16 +2,107 @@ package com.revature.hydra.curriculum.beans;
 
 import java.time.ZonedDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="SCHEDULED_DATE")
 public class ScheduledDate {
+	@Id
+	@SequenceGenerator(initialValue=0,
+					   allocationSize=1,
+					   name="SCHEDULED_DATE_ID_SEQ_GEN",
+					   sequenceName="SCHEDULED_DATE_ID_SEQ")
+	@GeneratedValue(generator="SCHEDULED_DATE_ID_SEQ_GEN", 
+					strategy=GenerationType.SEQUENCE)
+	@Column(name="ID")
 	private Integer id;
+	
+	@Column(name="WEEK")
+	@NotNull
 	private Integer week;
+	
+	@Column(name="DAY")
+	@NotNull
 	private Integer day;
+	
+	@Column(name="START_TIME")
+	@NotNull
 	private ZonedDateTime startTime;
+	
+	@Column(name="END_TIME")
+	@NotNull
 	private ZonedDateTime endTime;
 
 	public ScheduledDate() {
 		super();
 	}
+
+	public ScheduledDate(Integer id, Integer week, Integer day, ZonedDateTime startTime, ZonedDateTime endTime) {
+		super();
+		this.id = id;
+		this.week = week;
+		this.day = day;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getWeek() {
+		return week;
+	}
+
+	public void setWeek(Integer week) {
+		this.week = week;
+	}
+
+	public Integer getDay() {
+		return day;
+	}
+
+	public void setDay(Integer day) {
+		this.day = day;
+	}
+
+	public ZonedDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(ZonedDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public ZonedDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(ZonedDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	@Override
+	public String toString() {
+		return "ScheduledDate [(Schedule Date Id) \t id=" + id + ",\n"
+				+ "(Week) \t week=" + week + ",\n"
+				+ "(Day) \t day=" + day + ",\n"
+				+ "(Start Time) \t startTime=" + startTime + ",\n"
+				+ "(End Time) \t endTime=" + endTime + "\n]";
+	}
+	
+	
 	
 	
 }
---
+++ hydra-curriculum-service/src/main/java/com/revature/hydra/curriculum/beans/ScheduledSubtopic.java
@@ -0,0 +1,95 @@
+package com.revature.hydra.curriculum.beans;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="SCHEDULED_SUBTOPICS")
+public class ScheduledSubtopic {
+	@Id
+	@SequenceGenerator(initialValue=0,
+					   allocationSize=1,
+					   name="SCHEDULED_SUBTOPIC_ID_SEQ_GEN",
+					   sequenceName="SCHEDULED_SUBTOPIC_ID_SEQ")
+	@GeneratedValue(generator="SCHEDULED_SUBTOPIC_ID_SEQ_GEN",
+					strategy=GenerationType.SEQUENCE)
+	@Column(name="ID")
+	private Integer id;
+	
+	@Column(name="SUBTOPIC_ID")
+	@NotNull
+	private Integer subtopicId;
+	
+	@Column(name="SCHEDULED_DATE")
+	@NotNull
+	private ScheduledDate date;
+	
+	@Column(name="PARENT_CURRICULUM")
+	@ManyToOne(cascade=CascadeType.ALL,
+			   fetch=FetchType.LAZY)
+	@NotNull
+	private Curriculum parentCurriculum;
+	
+	public ScheduledSubtopic() {
+		super();
+	}
+
+	public ScheduledSubtopic(Integer id, Integer subtopicId, ScheduledDate date, Curriculum parentCurriculum) {
+		super();
+		this.id = id;
+		this.subtopicId = subtopicId;
+		this.date = date;
+		this.parentCurriculum = parentCurriculum;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getSubtopicId() {
+		return subtopicId;
+	}
+
+	public void setSubtopicId(Integer subtopicId) {
+		this.subtopicId = subtopicId;
+	}
+
+	public ScheduledDate getDate() {
+		return date;
+	}
+
+	public void setDate(ScheduledDate date) {
+		this.date = date;
+	}
+
+	public Curriculum getParentCurriculum() {
+		return parentCurriculum;
+	}
+
+	public void setParentCurriculum(Curriculum parentCurriculum) {
+		this.parentCurriculum = parentCurriculum;
+	}
+
+	@Override
+	public String toString() {
+		return "ScheduledSubtopic [(Scheduled Subtopic Id) \t id=" + id + ",\n"
+				+ "(Subtopic Id) \t subtopicId=" + subtopicId + ",\n"
+				+ "(Date) \t date=" + date + ",\n"
+				+ "(Parent Curriculum) \t parentCurriculum=" + parentCurriculum + "\n]";
+	}
+	
+	
+}
