91c346eda9f239d6b0ebf5a99499ae6f1c97b05f
Jennifer Fox <jrufox@gmail.com>
2018-04-18 10:13:22 -0400

    updating dev pipe

--- src/main/java/com/revature/repositories/ScheduledDateRepository.java
+++ src/main/java/com/revature/repositories/ScheduledDateRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.revature.beans.ScheduledDate;
 
 public interface ScheduledDateRepository extends JpaRepository<ScheduledDate, Integer>{
-
     public ScheduledDate findSechduledDateById(Integer id);
 }
